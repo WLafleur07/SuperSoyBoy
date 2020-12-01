dc8584562a1873ee5bf8b73dfdf8f13a9d4638ae
William Lafleur <williamlafleur@hotmail.ca>
2020-12-01 15:44:41 -0500

    Made the buzzsaw into a hazard and added particle system for death

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public GameObject playerDeathPrefab;
+    public AudioClip deathClip;
+    public Sprite hitSprite;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            var audioSource = GetComponent<AudioSource>();
+            if (audioSource != null && deathClip != null)
+            {
+                audioSource.PlayOneShot(deathClip);
+            }
+
+            Instantiate(playerDeathPrefab, collision.contacts[0].point, Quaternion.identity);
+
+            spriteRenderer.sprite = hitSprite;
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
