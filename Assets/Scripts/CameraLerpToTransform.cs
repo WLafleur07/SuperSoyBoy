9b34e84cfc1ec0a784547c4a658a004e1a32a562
William Lafleur <williamlafleur@hotmail.ca>
2020-12-10 18:22:40 -0500

    Imported two levels, cleaning them up so they are functional

--- Assets/Scripts/CameraLerpToTransform.cs
+++ Assets/Scripts/CameraLerpToTransform.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class CameraLerpToTransform : MonoBehaviour 
+public class CameraLerpToTransform : MonoBehaviour
 {
     public Transform camTarget;
     public float trackingSpeed;
+    public float cameraZDepth = -10f;
     public float minX;
     public float minY;
     public float maxX;
@@ -13,14 +13,11 @@ public class CameraLerpToTransform : MonoBehaviour
 
     void FixedUpdate()
     {
-        // checks valid transform component was assigned to camTarget
-        if(camTarget != null)
+        if (camTarget != null)
         {
-            // performs linear interpolation betweeen two vectors by the third parameter's value
             var newPos = Vector2.Lerp(transform.position, camTarget.position, Time.deltaTime * trackingSpeed);
             var camPosition = new Vector3(newPos.x, newPos.y, -10f);
             var v3 = camPosition;
-            // Clamp = contraints
             var clampX = Mathf.Clamp(v3.x, minX, maxX);
             var clampY = Mathf.Clamp(v3.y, minY, maxY);
             transform.position = new Vector3(clampX, clampY, -10f);
